b9af75504465ec014757f776c63df463f6800c86
Oskar <kogutoskar123@gmail.com>
2019-10-25 23:49:13 +0200

    Build

---
+++ Game jam Rzeszow 2019/Assets/Code/Resolution.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resolution : MonoBehaviour
+{
+    private void Awake()
+    {
+        Screen.SetResolution(1920, 1080, true);
+    }
+}
