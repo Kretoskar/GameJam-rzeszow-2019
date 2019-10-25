88b08ead6ebeac3b999bc278b1197c19c8de0275
Oskar <kogutoskar123@gmail.com>
2019-10-25 22:30:15 +0200

    Added main menu

---
+++ Game jam Rzeszow 2019/Assets/Code/MainMenuButtons.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public void NewGame()
+    {
+        PlayerPrefs.SetInt("People", 50);
+        PlayerPrefs.SetInt("Nature", 50);
+        PlayerPrefs.SetInt("Industry", 50);
+        PlayerPrefs.SetInt("Science", 50);
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
